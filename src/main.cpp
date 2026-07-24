decd6f03d3d4d8ff6ffccf3ec21829dcb14e34af
Alex-ELBR <alex@elbro.ru>
2026-07-24 14:52:30 +0300

    адрес дисплея исправлен с 0x3C<<1 на 0x3C

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ eDispatcher dispatcher;                  // объект диспетчера з
 eDisplay displ;
 eDS1338 rtc(&hi2c1, ADDRESS_RTC);        // обьект часов
 eEEPROM eeprom(&hi2c1, ADDRESS_EEPROM);  // обьект микросхемы памяти
-eOLED oled(&hi2c1, 0x3C << 1);           // обьект OLED дисплея 
+eOLED oled(&hi2c1, 0x3C);           // обьект OLED дисплея 
 
 eButton keyboard;
 eChannel channel[CHANNEL_AMOUNT];
