571f8922a35e11ceb2cd3dabf7ac52603284f28e
Alex-ELBR <alex@elbro.ru>
2026-07-24 14:49:08 +0300

    убрал лишнее в eOLED.cpp

--- lib/eOLED/eOLED.cpp
+++ lib/eOLED/eOLED.cpp
@@ -1,15 +1,10 @@
 #include "eOLED.hpp"
 #include <string.h>
 
-// Статический Си-указатель на I2C шину. 
-// Он свяжет изолированный Си-коллбэк с динамическим адресом из вашего C++ класса.
 static I2C_HandleTypeDef *current_i2c_bus = NULL;
-
-// Статические буферы для накопления байт перед отправкой пакета по I2C
 static uint8_t i2c_buffer[128];
 static uint8_t i2c_buf_idx;
 
-// Низкоуровневая Си-функция отправки данных, полностью очищенная от макросов user_ptr
 static uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
     switch(msg) {
         case U8X8_MSG_BYTE_INIT:
@@ -26,10 +21,8 @@ static uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int
             }
             break;
         case U8X8_MSG_BYTE_END_TRANSFER: {
-            // Если шина не привязана, выходим во избежание HardFault
             if (!current_i2c_bus) return 0;
             
-            // Прямая отправка через сохраненный адрес шины и макрос адреса дисплея
             HAL_I2C_Master_Transmit(current_i2c_bus, u8x8_GetI2CAddress(u8x8), i2c_buffer, i2c_buf_idx, 100);
             break;
         }
@@ -39,7 +32,6 @@ static uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int
     return 1;
 }
 
-// Коллбэк временных задержек под STM32 HAL
 static uint8_t u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
     switch(msg) {
         case U8X8_MSG_DELAY_MILLI:
@@ -51,14 +43,11 @@ static uint8_t u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_
     return 1;
 }
 
-// Конструктор класса
 eOLED::eOLED(I2C_HandleTypeDef *i2c_obj, uint16_t address) 
     : _i2c_bus(i2c_obj), _dev_address(address) {
 }
 
-// Функция инициализации дисплея
 void eOLED::init(void) {
-    // Сохраняем переданный указатель шины I2C в наш статический Си-контекст перед инициализацией
     current_i2c_bus = _i2c_bus;
 
     u8g2_Setup_ssd1306_i2c_128x64_noname_f(
@@ -73,7 +62,6 @@ void eOLED::init(void) {
     // Устанавливаем 8-битный адрес экрана (сдвиг влево на 1 бит)
     u8x8_SetI2CAddress(&_u8g2.u8x8, _dev_address << 1);
     
-    // Аппаратный запуск дисплея
     u8g2_InitDisplay(&_u8g2);
     u8g2_SetPowerSave(&_u8g2, 0); // Включаем экран
 }
@@ -92,35 +80,29 @@ void eOLED::periodic(void){
 }
 
 
-// Отрисовка вашего экрана сумерек
 void eOLED::show_time(const char* time) {
     u8g2_ClearBuffer(&_u8g2);
 
     /*
-    // 1. ДАТА (Сверху по центру)
     u8g2_SetFont(&_u8g2, u8g2_font_6x10_tf);
     u8g2_DrawStr(&_u8g2, 34, 10, date);
     */
 
-    // 2. ВРЕМЯ (Крупный шрифт по центру)
+    // ВРЕМЯ (Крупный шрифт по центру)
     u8g2_SetFont(&_u8g2, u8g2_font_fub20_tn);
     u8g2_DrawStr(&_u8g2, 24, 38, time);
 
     /*
-    // 3. НАЧАЛО СУМЕРЕК (Символ месяца)
     u8g2_SetFont(&_u8g2, u8g2_font_open_iconic_weather_2x_t);
-    // ИСПРАВЛЕНО: __u8g2 заменена на _u8g2
     u8g2_DrawGlyph(&_u8g2, 10, 62, 66); // 66 - код Луны
     
     u8g2_SetFont(&_u8g2, u8g2_font_6x10_tf);
     u8g2_DrawStr(&_u8g2, 32, 58, tw_start);
 
-    // 4. КОНЕЦ СУМЕРЕК (Символ солнца)
     u8g2_SetFont(&_u8g2, u8g2_font_open_iconic_weather_2x_t);
     u8g2_DrawGlyph(&_u8g2, 75, 62, 69); // 69 - код Солнца
 
     u8g2_SetFont(&_u8g2, u8g2_font_6x10_tf);
-    // ИСПРАВЛЕНО: __u8g2 заменена на _u8g2
     u8g2_DrawStr(&_u8g2, 97, 58, tw_end);
     */
 
